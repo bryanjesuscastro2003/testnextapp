f09f2afed28857b106be5951ba5f5544f15d8ac5
bryanjesuscastro2003 <jesusbryam@gmail.com>
2023-11-02 20:38:46 -0600

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hellbryan.js 👋</h1>
+    <p>
+      <Link href="/about">About</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
+               
